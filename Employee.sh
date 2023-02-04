e2aab806b871c7f47be3145b5663953050ffa096
Prachi <prachikale136@gmail.com>
2023-02-04 21:55:13 +0530

    Employee Wage function

--- Employee.sh
+++ Employee.sh
@@ -1,33 +1,36 @@
 #!/bin/bash -x
-
 IS_PART_TIME=1;
 IS_FULL_TIME=2;
 MAX_HRS_IN_MONTH=10;
 EMP_RATE_PER_HR=20;
 NUM_WORKING_DAYS=20;
 
-totalEmpHr=0;
+totalWorkHr=0;
 totalWorkingDays=0;
 
-while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && 
+function getWorkingHrs() {
+	case $1 in
+                $IS_FULL_TIME)
+                        workHrs=8
+                        ;;
+                $IS_PART_TIME)
+                        workHrs=4
+                        ;;
+                *)
+                        workHrs=0;
+                        ;;
+        esac
+	echo $workHrs;
+
+}
+
+while [[ $totalWorkHrs -lt $MAX_HRS_IN_MONTH && 
               $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 
 do
 	(( totalWorkingDays++ ))
-	empCheck=$((RANDOM%3));
-	case $empCheck in
-		$IS_FULL_TIME)
-			empHrs=8
-			;;
-		$IS_PART_TIME)
-			empHrs=4
-			;;
-		*)
-			empHrs=0;
-			;;
-	esac
-	totalEmpHr=$(($totalEmpHr*$empHrs));
-	
+	 workHrs="$( getWorkingHrs $((RANDOM%3)) )"
+	totalWorkHr=$(($totalWorkHr*$workHrs));
 done
-totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HR));
+totalSalary=$(($totalWorkHr*$EMP_RATE_PER_HR));
 
