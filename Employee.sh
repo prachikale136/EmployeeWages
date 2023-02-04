4d2152ec435eafad391192aeee3dffe4cd2dd724
Prachi <prachikale136@gmail.com>
2023-02-04 17:28:39 +0530

    Daily Wage Employee

--- Employee.sh
+++ Employee.sh
@@ -1,13 +1,14 @@
 echo "Welcome to Employee Wage computation program"
-
-
 #!/bin/bash -x
 isPresent=1;
 randomChecks=$((RANDOM%2));
 if [ $isPresent -eq $randomChecks ]
 then
-	echo "Employee is present";
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr));
+	echo $salary;
 else
-	echo "Employee is absent";
+	 echo $salary=0;
 fi
 
