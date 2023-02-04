481e70499c323654e9d94d93d77285d6784c3725
Prachi <prachikale136@gmail.com>
2023-02-04 21:27:39 +0530

    wages calculation

--- Employee.sh
+++ Employee.sh
@@ -1,26 +1,33 @@
-echo "Welcome to Employee Wage computation program";
+#!/bin/bash -x
 
-isPartTime=1;
-isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
-numWorkingDays=3;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-for(( days=1; days<=$numWorkingDays; day++ ))
+totalEmpHr=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH && 
+              $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+
 do
+	(( totalWorkingDays++ ))
 	empCheck=$((RANDOM%3));
-		case $empCheck in 
-			$isFullTime)
-				empHrs=8
-				;;
-			$isPartTime)
-				empHrs=4
-				;;
-			*)
-				empHrs=0
-				;;
-		esac
-
-salary=$(($empHrs*$empRatePerHr));
-totalSalary=$(($totalSalary+$salary));
+	case $empCheck in
+		$IS_FULL_TIME)
+			empHrs=8
+			;;
+		$IS_PART_TIME)
+			empHrs=4
+			;;
+		*)
+			empHrs=0;
+			;;
+	esac
+	totalEmpHr=$(($totalEmpHr*$empHrs));
+	
 done
+totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HR));
+
