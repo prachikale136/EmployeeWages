071d4be03f1fb3cdc5353e6b9c289c3af3dd0fbc
Prachi <prachikale136@gmail.com>
2023-02-04 17:12:44 +0530

    Employee Attendance

--- Employee.sh
+++ Employee.sh
@@ -1,1 +1,13 @@
 echo "Welcome to Employee Wage computation program"
+
+
+#!/bin/bash -x
+isPresent=1;
+randomChecks=$((RANDOM%2));
+if [ $isPresent -eq $randomChecks ]
+then
+	echo "Employee is present";
+else
+	echo "Employee is absent";
+fi
+
