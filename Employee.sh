a1a3ac82fb39a5840406a3eaa9f1f908b4d782bc
Prachi <prachikale136@gmail.com>
2023-02-04 20:30:50 +0530

    montly Employee wages Calculation

--- Employee.sh
+++ Employee.sh
@@ -1,21 +1,26 @@
-echo "Welcome to Employee Wage computation program"
-#!/bin/bash -x
+echo "Welcome to Employee Wage computation program";
+
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
+numWorkingDays=3;
 
-case $randomCheck in 
-	$isFullTime)
-		empHrs=8
-		;;
-	$isPartTime)
-		empHrs=4
-		;;
-	*)
-		empHrs=0
-		;;
-esac
+for(( days=1; days<=$numWorkingDays; day++ ))
+do
+	empCheck=$((RANDOM%3));
+		case $empCheck in 
+			$isFullTime)
+				empHrs=8
+				;;
+			$isPartTime)
+				empHrs=4
+				;;
+			*)
+				empHrs=0
+				;;
+		esac
 
 salary=$(($empHrs*$empRatePerHr));
-echo $salary;
+totalSalary=$(($totalSalary+$salary));
+done
